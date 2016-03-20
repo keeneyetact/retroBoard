330dcc804136ed33833dbc090c0a972c216961f7
keeneyetact <elaineelainefletcher@gmail.com>
2016-03-20 19:37:37 +0000

    Adding fastclick

--- ui.jsx
+++ ui.jsx
@@ -5,5 +5,8 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import ToolboxApp from 'react-toolbox/lib/app';
 import 'react-toolbox/lib/commons'
+import fastClick from 'fastclick';
+
+fastClick.attach(document.body);
 
 ReactDOM.render(<ToolboxApp><Index /></ToolboxApp>, document.getElementById('content'));
