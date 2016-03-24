753c616a00e8a50d310a686252b8bae07888a24f
keeneyetact <elaineelainefletcher@gmail.com>
2016-03-24 21:40:24 +0100

    Cleaning up App.jsx

--- app/pages/App.jsx
+++ app/pages/App.jsx
@@ -1,16 +1,11 @@
 import { default as React, PropTypes } from 'react';
 import Login from './Login';
-import Button from 'react-toolbox/lib/button';
-import AppBar from 'react-toolbox/lib/app_bar';
-import Navigation from 'react-toolbox/lib/navigation';
 import { connect } from 'react-redux';
 import { login } from '../state/user';
 import { initialise } from '../state/actions';
 import style from './App.scss';
-import Clients from './Clients';
-import Drawer from 'react-toolbox/lib/drawer';
 import icons from '../constants/icons';
-import translate from '../i18n/Translate';
+import TranslationProvider from '../i18n/TranslationProvider';
 import Header from './Header';
 
 class App extends React.Component {
@@ -23,7 +18,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <TranslationProvider>
                 <Header />
 
                 <br />
@@ -32,7 +27,7 @@ class App extends React.Component {
                 <br />
                 <br />
                 { this.renderLogin() }
-            </div>
+            </TranslationProvider>
         )
     }
 
@@ -47,12 +42,6 @@ class App extends React.Component {
 
     componentDidMount() {
         this.props.initialise(this.props.params.sessionId);
-    }
-
-    getChildContext() {
-        return {
-            currentLanguage: this.props.currentLanguage
-        };
     }
 }
 
@@ -69,11 +58,6 @@ App.defaultTypes = {
     onLogin: () => {},
     displayDrawerButton: true
 }
-
-App.childContextTypes = {
-    currentLanguage: PropTypes.string.isRequired
-};
-
 
 const stateToProps = state => ({
     user: state.user.name,
