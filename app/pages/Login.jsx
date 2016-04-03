509fbadd91fb6fd512bf3f595971566eb168a604
keeneyetact <elaineelainefletcher@gmail.com>
2016-04-03 16:34:53 +0100

    Icon issue

--- app/pages/Login.jsx
+++ app/pages/Login.jsx
@@ -3,6 +3,7 @@ import Button from 'react-toolbox/lib/button';
 import EnterInput from '../components/EnterInput';
 import translate from '../i18n/Translate';
 import LanguagePicker from '../components/LanguagePicker';
+import icons from '../constants/icons';
 
 @translate('Login')
 class Login extends Component {
@@ -12,7 +13,7 @@ class Login extends Component {
                 <div className="col-3-12"></div>
                 <div className="col-6-12">
                     <LanguagePicker />
-                    <EnterInput placeholder={this.props.strings.namePlaceholder} icon='people' onEnter={this.props.onLogin} ref="input" />
+                    <EnterInput placeholder={this.props.strings.namePlaceholder} icon={icons.people} onEnter={this.props.onLogin} ref="input" />
                     <Button label={this.props.strings.buttonLabel} accent raised onClick={() => {
                         const text = this.refs.input.value();
                         if (text) {
