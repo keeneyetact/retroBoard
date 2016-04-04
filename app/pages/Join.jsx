8fa2c6bc28e03ac1d4d82bc27fd83bf64cf531ac
keeneyetact <elaineelainefletcher@gmail.com>
2016-04-04 22:04:10 +0100

    Experimenting with the join page

--- app/pages/Join.jsx
+++ app/pages/Join.jsx
@@ -6,6 +6,7 @@ import Button from 'react-toolbox/lib/button';
 import { connect } from 'react-redux';
 import { createSession } from '../state/session';
 import translate from '../i18n/Translate';
+import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
 
 const stateToProps = state => ({ });
 
@@ -18,12 +19,36 @@ const actionsToProps = dispatch => ({
 class Join extends Component {
     render() {
         return (
+            <div style={{padding: 20 }}>
+            <Card raised>
+                <CardTitle>Welcome to Retrospected</CardTitle>
+                <CardText>
+                    Create a session right now by clicking:<br /><br />
+                <Button label={this.props.strings.newSession} accent raised onClick={this.props.createSession} />
+                <br /><br />
+                Or choose a custom name to your session:
+                <Input />
+                <Button label="Create custom session" accent raised onClick={this.props.createSession} />
+                </CardText>
+            </Card>
+        </div>
+        );
+    }
+    render2() {
+        return (
             <section className="grid">
                 <div className="col-3-12"></div>
                 <div className="col-6-12">
                     <Button label={this.props.strings.newSession} accent raised onClick={this.props.createSession} />
+                        <Card style={{width: '350px'}} raised>
+                            <CardTitle>Welcome to Retrospected</CardTitle>
+                            <CardText>Hello</CardText>
+                        </Card>
                 </div>
-                <div className="col-3-12"></div>
+                <div className="col-3-12">
+
+                </div>
+
             </section>
         );
     }
--- todo.md
+++ todo.md
@@ -1,4 +1,4 @@
 - unit tests
 - animations
 - readme: add links to frameworks
--
+- tooltip
