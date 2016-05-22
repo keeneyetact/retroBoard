6feec70d141ff7640e0e18d699b697c991a73698
keeneyetact <elaineelainefletcher@gmail.com>
2016-05-22 21:34:57 +0100

    Removed useless snackbar

--- app/pages/Main.jsx
+++ app/pages/Main.jsx
@@ -2,15 +2,12 @@ import React, { PropTypes } from 'react';
 import noop from 'lodash/noop';
 import flow from 'lodash/flow';
 import Component from '../Component';
-import translate from '../i18n/Translate';
 import { connect } from 'react-redux';
-import Snackbar from 'react-toolbox/lib/snackbar';
 import PostBoard from '../components/PostBoard';
 import SummaryBoard from '../components/SummaryBoard';
 import SessionName from '../components/SessionName';
 import { autoJoin } from '../state/session';
 import { getSummaryMode } from '../selectors';
-import icons from '../constants/icons';
 
 const stateToProps = state => ({
     summaryMode: getSummaryMode(state)
@@ -21,49 +18,19 @@ const actionsToProps = dispatch => ({
 });
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { snackBarActive: false };
-        this.mounted = false;
-    }
-
-    componentWillMount() {
-        this.setState({ snackBarActive: true });
-    }
 
     componentDidMount() {
         this.props.autoJoin(this.props.params.sessionId);
-        this.mounted = true;
-    }
-
-    componentWillUnmount() {
-        this.setState({ snackBarActive: false });
-        this.mounted = false;
     }
 
     render() {
-        const { summaryMode, strings } = this.props;
-        const hideSnackbar = () => {
-            if (this.mounted) {
-                this.setState({ snackBarActive: false });
-            }
-        };
+        const { summaryMode } = this.props;
         return (
             <div>
                 <div style={ { width: '100%', textAlign: 'center' } }>
                     <SessionName />
                 </div>
                 { summaryMode ? <SummaryBoard /> : <PostBoard /> }
-                <Snackbar
-                  action="Ok!"
-                  icon={ icons.question_answer }
-                  label={ strings.hint }
-                  type="accept"
-                  active={ this.state.snackBarActive }
-                  timeout={ 10000 }
-                  onClick={ hideSnackbar }
-                  onTimeout={ hideSnackbar }
-                />
             </div>
         );
     }
@@ -72,21 +39,16 @@ class Main extends Component {
 Main.propTypes = {
     autoJoin: PropTypes.func,
     summaryMode: PropTypes.bool,
-    strings: PropTypes.object,
     params: PropTypes.object
 };
 
 Main.defaultProps = {
     autoJoin: noop,
-    summaryMode: false,
-    strings: {
-        hint: 'You can invite others to this session by copy-pasting the URL'
-    }
+    summaryMode: false
 };
 
 const decorators = flow([
-    connect(stateToProps, actionsToProps),
-    translate('Main')
+    connect(stateToProps, actionsToProps)
 ]);
 
 export default decorators(Main);
