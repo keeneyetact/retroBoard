c32004ee79c2ebcafb0d73f04fefae4e3e2f5634
keeneyetact <elaineelainefletcher@gmail.com>
2016-06-29 12:52:31 +0100

    Fixing login screen

--- app/pages/Login.jsx
+++ app/pages/Login.jsx
@@ -6,32 +6,29 @@ import EnterInput from '../components/EnterInput';
 import translate from '../i18n/Translate';
 import LanguagePicker from '../components/LanguagePicker';
 import icons from '../constants/icons';
+import style from './Login.scss';
 
 class Login extends Component {
     render() {
         return (
-            <section className="grid">
-                <div className="col-3-12"></div>
-                <div className="col-6-12">
-                    <LanguagePicker />
-                    <EnterInput
-                      placeholder={this.props.strings.namePlaceholder}
-                      icon={icons.people}
-                      onEnter={this.props.onLogin} ref="input"
-                    />
-                    <Button label={this.props.strings.buttonLabel}
-                      accent
-                      raised
-                      onClick={() => {
-                          const text = this.refs.input.state.value;
-                          if (text) {
-                              this.props.onLogin(text);
-                          }
-                      }}
-                    />
-                </div>
-                <div className="col-3-12"></div>
-            </section>
+            <div className={style.login}>
+                <LanguagePicker />
+                <EnterInput
+                  placeholder={this.props.strings.namePlaceholder}
+                  icon={icons.people}
+                  onEnter={this.props.onLogin} ref="input"
+                />
+                <Button label={this.props.strings.buttonLabel}
+                  accent
+                  raised
+                  onClick={() => {
+                      const text = this.refs.input.state.value;
+                      if (text) {
+                          this.props.onLogin(text);
+                      }
+                  }}
+                />
+            </div>
         );
     }
 }
