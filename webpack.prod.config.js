c6b2b2ea036deea6c2d045bd43b88f96ad14810f
keeneyetact <elaineelainefletcher@gmail.com>
2016-08-08 21:26:07 +0100

    Changing limit to base64 SVGs to save space on the bundle

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -31,7 +31,7 @@ module.exports = {
         loaders: [
             { test: /\.css$/, loader: 'style!css' },
             { test: /(\.jsx|\.js)$/, loader: 'babel', exclude: /node_modules/ },
-            { test: /\.svg$/, loader: 'url?limit=10000' },
+            { test: /\.svg$/, loader: 'url?limit=10' },
             { test: /\.png$/, loader: 'url?limit=10000&mimetype=image/png' },
             { test: /\.jpg$/, loader: 'url?limit=10000&mimetype=image/jpeg' },
             { test: /\.json$/, loader: 'json-loader' },
