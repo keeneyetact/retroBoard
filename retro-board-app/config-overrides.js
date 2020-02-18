3af37a9a7497ef0efcad96f3d4894f8d6be74d2d
keeneyetact <elaineelainefletcher@gmail.com>
2020-02-18 18:29:02 +0400

    Adding plugin for styled-components

--- retro-board-app/config-overrides.js
+++ retro-board-app/config-overrides.js
@@ -1,8 +1,10 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 const rewireWorkspaces = require('react-app-rewire-yarn-workspaces');
+const rewireStyledComponents = require('react-app-rewire-styled-components');
 
-module.exports = function override(config, env) {
+module.exports = function overrideCra(config, env) {
   config = rewireReactHotLoader(config, env);
   config = rewireWorkspaces(config, env);
+  config = rewireStyledComponents(config, env);
   return config;
 };
