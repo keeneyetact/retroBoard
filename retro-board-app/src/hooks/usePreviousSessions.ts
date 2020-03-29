40749dd56f36d1173ac55592ead123563eea8c9f
keeneyetact <elaineelainefletcher@gmail.com>
2020-03-29 15:38:27 +0100

    Cache previous sessions

--- retro-board-app/src/hooks/usePreviousSessions.ts
+++ retro-board-app/src/hooks/usePreviousSessions.ts
@@ -3,16 +3,18 @@ import useUser from '../auth/useUser';
 import { fetchPreviousSessions } from '../api';
 import { useState, useEffect } from 'react';
 
+let CACHE: SessionMetadata[] = [];
+
 export default (): SessionMetadata[] => {
-  const [sessions, setSessions] = useState<SessionMetadata[]>([]);
+  const [sessions, setSessions] = useState<SessionMetadata[]>(CACHE);
   const user = useUser();
 
   useEffect(() => {
     async function load() {
       if (user && user.accountType !== 'anonymous') {
-        console.log('Loading sessions');
         const previousSessions = await fetchPreviousSessions();
         setSessions(previousSessions);
+        CACHE = previousSessions;
       }
     }
     load();
