c9d4e2ed5f103955894861cf8608ff54be9cfb31
keeneyetact <elaineelainefletcher@gmail.com>
2021-06-27 09:50:45 +0100

    Fixing Frontend Docker script (#279)

--- frontend/docker/frontend-entrypoint.sh
+++ frontend/docker/frontend-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${BACKEND_HOST:-backend} ${BACKEND_PORT:-3201}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+BACKEND_HOST="${BACKEND_HOST:-backend}" \
+BACKEND_PORT="${BACKEND_PORT:-3201}" \
+envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Replacing Google Analytics and Sentry IDs. Separator is # because the sentry URL contains a /
 sed -i "s#NO_GA#${GA_ID:-}#g" /usr/share/nginx/html/index.html
