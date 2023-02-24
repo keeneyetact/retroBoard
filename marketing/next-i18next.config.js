ffed7ad582e146290212c8f30c74388d98d2e199
keeneyetact <elaineelainefletcher@gmail.com>
2023-02-24 10:00:33 +0000

    Marketing Translations (beta) (#477)

--- marketing/next-i18next.config.js
+++ marketing/next-i18next.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'fr'],
+    locales: ['en', 'fr', 'de', 'nl'],
+    domains: [
+      {
+        domain: 'retrospected.com',
+        defaultLocale: 'en',
+      },
+      {
+        domain: 'retrospected.fr',
+        defaultLocale: 'fr',
+      },
+      {
+        domain: 'retrospected.de',
+        defaultLocale: 'de',
+      }
+    ],
   },
 }
