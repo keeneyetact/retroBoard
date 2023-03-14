08d48e7a94800aceaf43aefe3db530686800db14
keeneyetact <elaineelainefletcher@gmail.com>
2023-03-14 12:44:49 +0000

    German translations (#497)

--- marketing/next-i18next.config.js
+++ marketing/next-i18next.config.js
@@ -3,7 +3,7 @@ const subDomain = process.env.NEXT_BACKEND_SUB_DOMAIN;
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'fr'], // , 'de', 'nl'],
+    locales: ['en', 'fr', 'de'], // , 'de', 'nl'],
     domains: [
       {
         domain: `${subDomain}.retrospected.com`,
@@ -13,10 +13,10 @@ module.exports = {
         domain: `${subDomain}.retrospected.fr`,
         defaultLocale: 'fr',
       },
-      // {
-      //   domain: `${subDomain}.retrospected.de`,
-      //   defaultLocale: 'de',
-      // }
+      {
+        domain: `${subDomain}.retrospected.de`,
+        defaultLocale: 'de',
+      }
     ],
   },
 }
